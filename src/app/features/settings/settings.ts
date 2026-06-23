fb7d25f0d4d9fb9075e9eb63abcb2926a95e2350
Elvin Acuña <elvinwaz@outlook.com>
2026-06-22 21:36:48 -0600

    feat: Add i18n keys and accessibility improvements

--- src/app/features/settings/settings.ts
+++ src/app/features/settings/settings.ts
@@ -1,9 +1,9 @@
 import { Component, inject, signal, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
